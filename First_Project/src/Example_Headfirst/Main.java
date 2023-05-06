e2790d08644c89e7f1714fd8896f1e131d3e7872
Binawei <111076088+Binawei@users.noreply.github.com>
2023-05-06 11:59:16 -0700

    Delete First_Project directory

--- First_Project/src/Example_Headfirst/Main.java
+++
@@ -1,9 +0,0 @@
-package Example_Headfirst;
-
-public class Main {
-    public static void main(String[] args)
-    {
-        Poor_Dog Puppy = new Poor_Dog();
-        System.out.println("The Puppy name is :"+ Puppy.getName()+" and age is :"+ Puppy.getSize());
-    }
-}
